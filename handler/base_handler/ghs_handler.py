c3f404d89ff0babd9a4f929df182f73e7b1bf97a
yowasa_windows <2508488843@qq.com>
2019-12-06 00:36:45 +0800

    调整图源

--- handler/base_handler/ghs_handler.py
+++ handler/base_handler/ghs_handler.py
@@ -59,7 +59,7 @@ def ghs_pixiv(content):
     try:
         # 没有数据从日排行前三十里随机取一张
         results = api.illust_ranking(mode='day_r18', date=None, offset=None)
-        return package_pixiv_img(results.illusts[random.randint(0, 29)])
+        return package_pixiv_img(results.illusts[random.randint(0, len(results.illusts)-1)])
     except:
         api.login("2508488843@qq.com", "czqq872710284")
 
@@ -89,20 +89,51 @@ def package_img(url):
 
 
 def package_pixiv_img(illust):
-    url = illust.image_urls.large
-    name = url[url.rfind("/") + 1:]
-    api.download(url, path=cq_image_file,replace=True)
-    name = trance_png(name,cq_image_file)
-    return f'[CQ:image,file={name}]'
+    url = illust.meta_single_page.get('original_image_url')
+    if not url:
+        urls=[]
+        for i in illust.meta_pages:
+            uu=i.get('image_urls').get('large')
+            if uu:
+                urls.append(uu)
+        img_list=[]
+        for uurl in urls:
+            name = uurl[uurl.rfind("/") + 1:]
+            api.download(uurl, path=cq_image_file, replace=True)
+            name = trance_png(name, cq_image_file)
+            img_list.append(f'[CQ:image,file={name}]')
+        return ''.join(img_list)
+    else:
+        if 'gif' not in url:
+            url=illust.image_urls.get('large')
+        name = url[url.rfind("/") + 1:]
+        api.download(url, path=cq_image_file,replace=True)
+        name = trance_png(name,cq_image_file)
+        return f'[CQ:image,file={name}]'
 
 def trance_png(name,cq_image_file):
     im = Image.open(cq_image_file+name)
-    name=name.replace("jpg","png")
-    try:
-        im.save(cq_image_file+name, "PNG")
-    except IOError:
-        pass
-    return name
+    if im.format =='WEBP':
+        name = name.replace("jpg", "png")
+        try:
+            w, h = im.size
+            w_s,h_s=float_range(w,h)
+            im=im.resize((w_s, h_s))
+            im.save(cq_image_file + name, "PNG")
+        except IOError:
+            pass
+        return name
+    elif im.format in ['JPEG','PNG']:
+        try:
+            w, h = im.size
+            w_s,h_s=float_range(w,h)
+            im=im.resize((w_s, h_s))
+            im.save(cq_image_file + name)
+        except IOError:
+            pass
+        return name
+    else:
+        return name
 
 
 def ten_page_search(cmd_msg):
@@ -118,3 +149,7 @@ def ten_page_search(cmd_msg):
     fetch = random.randint(0, fetch)
     return illusts_sorted[fetch]
 
+#得到正负10%的晃动比例，生成新size
+def float_range(x,y):
+    level = round(random.random() * 0.2 + 0.9, 2)
+    return int(x * level),int(y * level)
--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-from pixivpy3 import *
+import random
+from PIL import Image
+#得到正负10%的晃动比例，生成新size
+def float_range(x,y):
+    level = round(random.random() * 0.2 + 0.9, 2)
+    return int(x * level),int(y * level)
 
-api = AppPixivAPI()
-
-api.login("2508488843@qq.com","czqq872710284")
-
-results=api.illust_ranking( mode='day_r18', date=None, offset=None)
-
-
-api.download(results.illusts[20].image_urls.square_medium)
-
+im = Image.open('78132341_p0_master1200.jpg')
+w, h = im.size
+w_s,h_s=float_range(w,h)
+im=im.resize((w_s, h_s))
+im.save('test.png', "PNG")
